40f298f2d661c7136174a92a4d18119f38b9543f
Tuana <tuanaaunall10@gmail.com>
2026-07-29 14:19:40 +0300

    Initial commit: Proje altyapisi kuruldu ve kutuphaneler iceri aktarildi

---
+++ main_car.py
@@ -0,0 +1,26 @@
+"""
+Problem tanimi: video uzerinden yolo ile arac sayma
+    - car
+    - truck
+    - otobus
+
+YOLO ile arac tracking sonrasinde belirli bir cizgiyi gecen araclarin sayisini count edelim
+
+data: https://www.kaggle.com/datasets/benjaminguerrieri/car-detection-videos?select=IMG_5268.MOV
+"""
+
+# import libraries
+import cv2  # opencv
+import numpy as np
+from ultralytics import YOLO
+
+# yardimci fonksiyon tanimlama
+
+
+# meodeli tanimlama
+
+
+# video cacpture
+
+
+# cizgi tanimlama
